c0d05bd2a49c835f52e6bff067a8d5301644e1fa
Giorgos Mellios <82503972+giomellios@users.noreply.github.com>
2022-11-11 05:00:10 +0200

    Update toJSONinterface.java

--- src/Classes/toJSONinterface.java
+++ src/Classes/toJSONinterface.java
@@ -1,6 +1,10 @@
 package Classes;
 
+import java.io.IOException;
+
 public interface toJSONinterface<T> {
 
 	public String toJSON(T t);
+	public void Request(String address,String content) throws IOException, InterruptedException;
 }
+
